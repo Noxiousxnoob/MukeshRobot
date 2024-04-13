9fed24968d6a63876aefbd8a039570cbacca44f5
Noxiousxnoob <154877712+Noxiousxnoob@users.noreply.github.com>
2024-04-13 23:18:08 +0530

    Update bhagwatGeeta.py

--- MukeshRobot/modules/bhagwatGeeta.py
+++ MukeshRobot/modules/bhagwatGeeta.py
@@ -2,4 +2,4 @@
 from MukeshAPI import api
 verse_data = api.bhagwatgita(1, 5)
 print(verse_data)
-@Mukesh.on_message(filters.command(["bg"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))]
+@Mukesh.on_message(filters.command(["bg"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
