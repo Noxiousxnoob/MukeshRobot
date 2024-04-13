7d58aeeeb3f3315498d43f1bf87242f1c7fd6886
Noxiousxnoob <154877712+Noxiousxnoob@users.noreply.github.com>
2024-04-13 23:20:41 +0530

    Update bhagwatGeeta.py

--- MukeshRobot/modules/bhagwatGeeta.py
+++ MukeshRobot/modules/bhagwatGeeta.py
@@ -3,3 +3,8 @@ from MukeshAPI import api
 verse_data = api.bhagwatgita(1, 5)
 print(verse_data)
 @Mukesh.on_message(filters.command(["bg"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
+async def bhagwat_Geeta(bot, message):
+    
+    try:
+        await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+        if len(message.command) < 2:
