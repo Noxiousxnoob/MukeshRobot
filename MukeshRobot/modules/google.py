9a72f428324b11adb36a191afe6502d1791b3ce1
Noxiousxnoob <154877712+Noxiousxnoob@users.noreply.github.com>
2024-05-28 10:46:40 +0530

    Update google.py

--- MukeshRobot/modules/google.py
+++ MukeshRobot/modules/google.py
@@ -60,7 +60,7 @@ async def img_sampler(event):
     jit = f'"{query}"'
     downloader.download(
         jit,
-        limit=20,
+        limit=5,
         output_dir="store",
         adult_filter_off=True,
         force_replace=False,
