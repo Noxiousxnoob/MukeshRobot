36579ab779715d58d8371c073c2f9ed3af91c4bf
Noxiousxnoob <154877712+Noxiousxnoob@users.noreply.github.com>
2024-06-08 10:55:14 +0530

    Update aiimagee.py

--- MukeshRobot/modules/aiimagee.py
+++ MukeshRobot/modules/aiimagee.py
@@ -1,9 +1,5 @@
 from MukeshAPI import api
 @Mukesh.on_message(filters.command("imaginee"))
-async def imagine_(b, message: Message):
-    if message.reply_to_message:
-        text = message.reply_to_message.text
-    else:
 response = api.ai_image("cute boy pic")
 #print(response)
 with open("mukesh.jpg", 'wb') as f:
