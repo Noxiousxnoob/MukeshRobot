b7cd769b41b21e0039f403c8bc845a619925b663
Noxiousxnoob <154877712+Noxiousxnoob@users.noreply.github.com>
2024-06-08 10:56:24 +0530

    Update aiimagee.py

--- MukeshRobot/modules/aiimagee.py
+++ MukeshRobot/modules/aiimagee.py
@@ -1,4 +1,10 @@
+from pyrogram import filters
+from pyrogram.types import  Message
+from pyrogram.types import InputMediaPhoto
+from .. import pbot as  Mukesh,BOT_USERNAME
 from MukeshAPI import api
+from pyrogram.enums import ChatAction,ParseMode
+
 @Mukesh.on_message(filters.command("imaginee"))
 response = api.ai_image("cute boy pic")
 #print(response)
